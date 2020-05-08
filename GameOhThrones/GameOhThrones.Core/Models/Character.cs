98c177ef49d3e7734e365ba2213d751f816bf98a
Várhidi Bence <47637990+varhidibence@users.noreply.github.com>
2020-05-08 02:57:42 -0700

    Model classes

---
+++ GameOhThrones/GameOhThrones.Core/Models/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOhThrones.Core.Models
+{
+
+    public class Character
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string culture { get; set; }
+        public string born { get; set; }
+        public string died { get; set; }
+        public List<string> titles { get; set; }
+        public List<string> aliases { get; set; }
+        public Character father { get; set; }
+        public Character mother { get; set; }
+        public List<Character> spouse { get; set; }
+        public List<House> allegiances { get; set; }
+        public List<Book> books { get; set; }
+        public List<Book> povBooks { get; set; }
+        public List<string> tvSeries { get; set; }
+        public List<string> playedBy { get; set; }
+    }
+
+}
---
+++ GameOhThrones/GameOhThrones.Core/Models/House.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOhThrones.Core.Models
+{
+
+    public class House
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+        public string region { get; set; }
+        public string coatOfArms { get; set; }
+        public string words { get; set; }
+        public List<string> titles { get; set; }
+        public List<string> seats { get; set; }
+        public Character currentLord { get; set; }
+        public Character heir { get; set; }
+        public House overlord { get; set; }
+        public string founded { get; set; }
+        public Character founder { get; set; }
+        public string diedOut { get; set; }
+        public List<string> ancestralWeapons { get; set; }
+        public List<House> cadetBranches { get; set; }
+        public List<Character> swornMembers { get; set; }
+    }
+
+}
